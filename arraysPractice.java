0d6dce353bd1e8ca8b6d127371533a2747e5fe5d
stevejj4 <jumaomondi16@gmail.com>
2024-05-16 19:54:26 +0300

    Arrays practice

---
+++ arraysPractice.java
@@ -0,0 +1,11 @@
+public class arraysPractice {
+    public static void main (String [] args){
+        int[] intArray = new int[20];
+        for(int i = 0; i<intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+
+
+    }
+
+}
