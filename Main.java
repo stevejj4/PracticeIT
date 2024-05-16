4a9980279ec6f9b4f0db9c68813b77af8fe09c87
stevejj4 <jumaomondi16@gmail.com>
2024-05-16 22:11:26 +0300

    New code

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main (String [] args) {
+        int[] arr = new int[]{1,2,3};
+        for(int i = 0; i<3; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
