6c52042136ab1f816706d9547b43b8fcab2e688d
stevejj4 <jumaomondi16@gmail.com>
2024-05-16 20:18:34 +0300

    prints the last element in the array

--- arraysPractice.java
+++ arraysPractice.java
@@ -2,7 +2,8 @@ public class arraysPractice {
     public static void main (String [] args){
         int[] intArray = new int[20];
         for(int i = 0; i<intArray.length; i++) {
-            System.out.print(intArray[i] + " ");
+            /*System.out.print(intArray[i] + " ");*/
+            System.out.println(intArray.length - 1);
             
         }
         System.out.println("The length of the arrays is" + " " + intArray.length);
